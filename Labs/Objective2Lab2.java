b54d250f6e6310d20c0f6da86c119c87b957e201
Angel <angel1179008@gmail.com>
2022-02-27 21:27:11 -0700

    Completed Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Paterson";
+    int age = 47;
+    String state = "Arkansas";
+    System.out.println("Charles Paterson, 47 Arkansas");
+
+  }
+}

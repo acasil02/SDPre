15d9e68cb3c513477b87b9ad918a6e65a7faa4bb
Angel <angel1179008@gmail.com>
2022-03-01 20:12:00 -0700

    Completed Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int sum, count;
+
+    sum =10;
+    count = 0;
+
+    while (count < 210) {
+      count = sum + count;
+      }
+        System.out.println(count);
+
+      }
+    }

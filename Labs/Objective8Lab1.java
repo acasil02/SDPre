a213b50b529fffc378aa78b19a028f311f63b3a5
Angel <angel1179008@gmail.com>
2022-03-02 17:10:21 -0700

    Completed Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int n = 1; n <= 10; n++) {
+      System.out.println(n);
+    }
+
+    for (int n = 10; n >= 1; n--) {
+      System.out.println(n);
+    }
+  }
+}
